28662065de6124d98ddf62ee41b29d78a47f0b2e
akihisa <sahikia117@gmail.com>
2024-10-13 20:17:24 +0900

    annotate追加とアノテーション作成

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id          :bigint           not null, primary key
+#  author_name :string(255)
+#  title       :string(255)
+#  body        :text(65535)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Board < ApplicationRecord
 end
